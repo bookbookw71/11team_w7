d2fb57c2b8f4e4b2dab170b74f12b91c0d56535b
sungheeHong <110082072+sungheeHong@users.noreply.github.com>
2022-09-09 11:52:47 +0900

    create

---
+++ src/main/java/com/example/bookbookw71/Bookbookw71Application.java
@@ -0,0 +1,13 @@
+package com.example.bookbookw71;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookbookw71Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bookbookw71Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookbookw71/Bookbookw71ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookbookw71;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookbookw71ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
