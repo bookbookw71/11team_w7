054fdcd8ac779a49201c636d1f3c4b815b897367
sungheeHong <110082072+sungheeHong@users.noreply.github.com>
2022-09-13 21:47:51 +0900

    merge 빠진 부분 직접 추가

---
+++ src/main/java/com/example/bookbookw71/search/Item.java
@@ -0,0 +1,13 @@
+package com.example.bookbookw71.search;
+
+public class Item {
+
+    public String title = "";
+    public String author = "";
+    public String description = "";
+    public String imageUrl = "";
+    public String link = "";
+
+    public int startIndex;
+
+}
