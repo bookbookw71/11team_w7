f9b37e21c658670e073bc04be77bc06991b65be6
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2022-09-15 01:32:15 +0900

    오류 수정

--- src/main/java/com/example/bookbookw71/service/MemberService.java
+++ src/main/java/com/example/bookbookw71/service/MemberService.java
@@ -60,21 +60,18 @@ public class MemberService {
         Member member = isPresentMember(requestDto.getEmail());
         HttpHeaders headers = new HttpHeaders();
 
-        System.out.println("0000");
         if (null == member) {
             Message message = new Message();
             message.setStatus(StatusEnum.MEMBER_NOT_FOUND);
             message.setMessage("사용자를 찾을 수 없습니다.");
             return new ResponseEntity<>(message,headers,HttpStatus.BAD_REQUEST);
         }
-        System.out.println("1111");
         if (!member.validatePassword(passwordEncoder, requestDto.getPassword())) {
             Message message = new Message();
             message.setStatus(StatusEnum.INVALID_MEMBER);
             message.setMessage("사용자를 찾을 수 없습니다.");
             return new ResponseEntity<>(message,headers,HttpStatus.BAD_REQUEST);
         }
-        System.out.println("22222");
 
         TokenDto tokenDto = tokenProvider.generateTokenDto(member);
         tokenToHeaders(tokenDto, response);
