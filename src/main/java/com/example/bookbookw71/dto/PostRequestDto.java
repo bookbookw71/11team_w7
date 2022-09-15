757eb11771301e15e0326d806fdc5eacc7c47f5e
sungheeHong <110082072+sungheeHong@users.noreply.github.com>
2022-09-15 14:22:08 +0900

    post 작성 정규표현식 추가

--- src/main/java/com/example/bookbookw71/dto/PostRequestDto.java
+++ src/main/java/com/example/bookbookw71/dto/PostRequestDto.java
@@ -3,25 +3,36 @@ package com.example.bookbookw71.dto;
 import com.example.bookbookw71.model.Member;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PostRequestDto {
-    private long id;
 
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String content;
 
+    @NotBlank
     private String imageUrl;
 
+    @NotBlank
     private int bookPage;
 
+    @NotBlank
     private int star;
 
+    @NotBlank
+    @Pattern(regexp = "^\\d{4}\\/\\d{2}\\/\\d{2}$")
     private String readStart;
 
+    @NotBlank
+    @Pattern(regexp = "^([0-9]{4})\\/([0-9]{2})\\/([0-9]{2})$")
     private String readEnd;
 }
--- src/main/java/com/example/bookbookw71/search/SearchController.java
+++ src/main/java/com/example/bookbookw71/search/SearchController.java
@@ -1,11 +1,7 @@
 package com.example.bookbookw71.search;
 
-import com.example.bookbookw71.controller.response.ResponseDto;
-import com.example.bookbookw71.dto.PostRequestDto;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
